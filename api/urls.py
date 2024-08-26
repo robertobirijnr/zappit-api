90d70e163f5da3bb25ef65adc7a0aee811f71423
Robert Admin <robert.admin@eglgh.com>
2024-08-26 09:06:41 +0000

    auth

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('todos/complete', views.TodoCompletedList.as_view()),
+    path('todos', views.TodoListCreate.as_view()),
+    path('todos/<int:pk>', views.TodoUpdateDelete.as_view()),
+     path('todos/<int:pk>/complete', views.TodoComplete.as_view()),
+    path('todos/completed', views.TodoCompletedList.as_view()),
+
+    path('signup', views.signup),
+    path('login', views.login),
+     
+
 ]
